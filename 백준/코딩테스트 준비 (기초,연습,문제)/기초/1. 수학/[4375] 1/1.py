d9ad7837bf9018451f4d760797ccb360f78b48b8
junho <jo62698476@gmail.com>
2024-02-18 22:15:23 +0900

    add: [4375] 1 추가

---
+++ 백준/코딩테스트 준비 (기초,연습,문제)/기초/1. 수학/[4375] 1/1.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    try:
+        n = int(input())  # 정수 n
+    except:
+        break
+
+    num = 1
+    count = 1
+    while True:
+        if num % n != 0:
+            num = num * 10 + 1
+            count += 1
+        else:
+            break
+
+    print(count)
---
+++ 백준/코딩테스트 준비 (기초,연습,문제)/기초/README.md
@@ -0,0 +1,3 @@
+# 코딩테스트 준비 - 기초
+[https://code.plus/course/51](https://code.plus/course/51)  
+위 사이트에서 제공하는 문제 풀이
